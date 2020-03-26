afe315caa71973444cc1778168c90b0f2f41f17b
cmontellanob <carlosmontellano@gmail.com>
2020-03-26 16:48:05 -0400

    26/03/20

---
+++ RMIFactorial/src/Cliente.java
@@ -0,0 +1,20 @@
+
+import java.rmi.*;
+
+public class Cliente{
+    public static void main(String args[]){
+	InterfazOperaciones calculadora;
+      int numero;
+	try {
+	    calculadora=(InterfazOperaciones)Naming.lookup("rmi://localhost/Calculadora");
+            numero=5;
+	    System.out.println();
+	    System.out.println( "   "+numero+"! = "+calculadora.Factorial(numero));
+	    System.out.println();
+	}
+	catch (Exception e){
+	    e.printStackTrace();
+	}
+    }
+}
+
