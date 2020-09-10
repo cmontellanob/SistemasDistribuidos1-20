60f10d5edc4ad16004f89731ac5f05394d6fc112
John Doe <carlosmontellano@gmail.com>
2020-09-10 18:12:04 -0400

    2doparcial

---
+++ 2doParcial/ClienteVotacion/ClienteVotacion/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ClienteVotacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbSigla = new System.Windows.Forms.ComboBox();
+            this.cbFormato = new System.Windows.Forms.ComboBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sigla";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Obtener";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Formato";
+            // 
+            // cbSigla
+            // 
+            this.cbSigla.FormattingEnabled = true;
+            this.cbSigla.Items.AddRange(new object[] {
+            "CC",
+            "FPV",
+            "MTS",
+            "UCS",
+            "MAS–IPSP",
+            "21F",
+            "PDC",
+            "MNR",
+            "PAN-BOL"});
+            this.cbSigla.Location = new System.Drawing.Point(197, 26);
+            this.cbSigla.Name = "cbSigla";
+            this.cbSigla.Size = new System.Drawing.Size(121, 21);
+            this.cbSigla.TabIndex = 3;
+            // 
+            // cbFormato
+            // 
+            this.cbFormato.FormattingEnabled = true;
+            this.cbFormato.Items.AddRange(new object[] {
+            "votos",
+            "porcentaje"});
+            this.cbFormato.Location = new System.Drawing.Point(197, 74);
+            this.cbFormato.Name = "cbFormato";
+            this.cbFormato.Size = new System.Drawing.Size(121, 21);
+            this.cbFormato.TabIndex = 4;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(225, 226);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "()";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cbFormato);
+            this.Controls.Add(this.cbSigla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbSigla;
+        private System.Windows.Forms.ComboBox cbFormato;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ 2doParcial/ClienteVotacion/ClienteVotacion/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteVotacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicioVotacion.WSVotacionClient wsvotacion = new ServicioVotacion.WSVotacionClient();
+            String sigla = cbSigla.Text;
+            String formato = cbFormato.Text; 
+            double resultado=wsvotacion.obtenerVotacion(sigla,formato);
+            lblResultado.Text = resultado.ToString();
+        }
+    }
+}
